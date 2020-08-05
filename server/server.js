7f245ba8caad1734f59af63b4de3d98482334e74
dylandasilva1999 <55394640+dylandasilva1999@users.noreply.github.com>
2020-08-05 15:42:20 +0200

    Add API Call for Classes
    -Added API call with the app.get function to fetch the list of classes in the local data.js file.

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ var express = require('express');
 var app = express();
 var port = 8000;
 var logger = require('./logger');
+var data = require('./data');
 
 var urlpath = path.join(__dirname, '../frontend/build/');
 
@@ -25,4 +26,19 @@ app.get('/home', (request, response) => {
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
-})
+});
+
+/*
+List of all the classes
+*/
+app.get('/api/classes/', function (request, response) {
+    response.json(data.classes);
+});
+
+/*
+Details of particular class
+a.The teacher of the class
+*/
+app.get('/api/classes/', function (request, response) {
+    response.json(data.classes);
+});
